e18868ff1ff03671a7c4489fc0e06051e0db4eb2
Riley Rohloff <rileyrohloff@workfront.com>
2019-09-23 09:58:29 -0600

    first commit

---
+++ tests/test_ch2.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+import time
+
+
+def test_ice_spirit():
+    driver = webdriver.Chrome()
+    driver.get('https://statsroyale.com/')
+
+    driver.find_element(By.CSS_SELECTOR, "[href*='/cards']").click()
+    time.sleep(2)
+    ice_spirit = driver.find_element(By.CSS_SELECTOR, "[href*='Ice+Spirit']")
+
+    assert ice_spirit.is_displayed()
+
+def test_lava_hound():
+    driver = webdriver.Chrome()
+
+    driver.get('https://statsroyale.com/')
+
+    driver.find_element(By.CSS_SELECTOR, "[href*='/cards']").click()
+    time.sleep(2)
+    lava_hound = driver.find_element(By.CSS_SELECTOR, "[href*='Lava+Hound']")
+    assert lava_hound.is_displayed()
