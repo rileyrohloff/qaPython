d630cdc327b7befda9a3eb62412b1c35e20c298b
Riley Rohloff <rileyrohloff@workfront.com>
2019-09-23 15:04:58 -0600

    ready for pull status

--- tests/test_ch2.py
+++ tests/test_ch2.py
@@ -23,3 +23,16 @@ def test_lava_hound():
     time.sleep(2)
     lava_hound = driver.find_element(By.CSS_SELECTOR, "[href*='Lava+Hound']")
     assert lava_hound.is_displayed()
+
+
+def test_common_cards_only():
+    driver = webdriver.Chrome()
+
+    driver.get('https://statsroyale.com/')
+
+    driver.find_element(By.CSS_SELECTOR, "[href*='/cards']").click()
+    time.sleep(2)
+    ice_spirit = driver.find_element(By.CSS_SELECTOR, "[href*='Ice+Spirit']")
+    commonBox = driver.find_element(By.CSS_SELECTOR, "[name*='common-cards']").click()
+
+    assert ice_spirit.is_displayed() == False
